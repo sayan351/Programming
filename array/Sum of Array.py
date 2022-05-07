ac97315b94dbb4b9e9d30a0ee26994fe97777dc4
sayan351 <105107050+sayan351@users.noreply.github.com>
2022-05-08 02:06:45 +0530

    Create Sum of Array.py

---
+++ array/Sum of Array.py
@@ -0,0 +1,8 @@
+Problem description: https://practice.geeksforgeeks.org/problems/sum-of-array2326/1/?page=1&difficulty[]=-2&category[]=Arrays&category[]=Strings&sortBy=submissions#
+Time complexity: o(n)
+Space complexity: o(1)
+def _sum(self,arr, n):
+	    sum=0
+	    for i in arr:
+	        sum=sum+i
+	    return sum
